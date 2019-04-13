1284732b87737337f352ec1a00d6fb998c1c2a70
Michael <michaelmcdaid0@gmail.com>
2019-04-13 02:06:31 +0100

    security update

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import os
 
 from api import create_app
 
-config_name = os.getenv('APP_SETTINGS') # config_name = "development" change this when we go to AWS EB
+config_name = os.getenv('APP_SETTINGS')  # config_name = "development" change this when we go to AWS EB
 application = create_app(config_name)
 
 if __name__ == '__main__':
